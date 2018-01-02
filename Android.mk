d13a8fba26cbda182d5c3c36d9294fdf23f8cf00
XiNGRZ <chenxingyu92@gmail.com>
2018-01-02 10:51:56 +0800

    msm8998-common: Disable cheeseburger until it's commonized
    Change-Id: I472097ffd7d340bcd10f4fac2160c48a8e927d85

--- Android.mk
+++ Android.mk
@@ -16,7 +16,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter cheeseburger dumpling,$(TARGET_DEVICE)),)
+ifneq ($(filter dumpling,$(TARGET_DEVICE)),)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := IFAAService
